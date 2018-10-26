ef33c6b404fbdeedd6705af9362137b1767f741a
Roman-Port <darkwings220@gmail.com>
2018-10-25 22:19:00 -0500

    we're gonna need a bigger buffer, pal

--- WebPebble-QEMU/WebPebble-QEMU/Program.cs
+++ WebPebble-QEMU/WebPebble-QEMU/Program.cs
@@ -18,7 +18,7 @@ namespace WebPebble_QEMU
             QemuSession s = null;
             try
             {
-                s = QemuSession.SpawnSession(11);
+                s = QemuSession.SpawnSession(12);
             } catch (Exception ex)
             {
                 Console.WriteLine(ex.Message + "@" + ex.StackTrace);
