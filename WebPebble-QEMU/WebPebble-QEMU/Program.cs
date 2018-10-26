dd1c9d622989487d0656e8f3530f870562cde386
Roman-Port <darkwings220@gmail.com>
2018-10-25 20:05:19 -0500

    Attempted bugfix to fix "address already in use" errors.

--- WebPebble-QEMU/WebPebble-QEMU/Program.cs
+++ WebPebble-QEMU/WebPebble-QEMU/Program.cs
@@ -18,7 +18,7 @@ namespace WebPebble_QEMU
             QemuSession s = null;
             try
             {
-                s = QemuSession.SpawnSession(1);
+                s = QemuSession.SpawnSession(3);
             } catch (Exception ex)
             {
                 Console.WriteLine(ex.Message + "@" + ex.StackTrace);
